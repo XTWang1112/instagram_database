4f81a73ecc7b64e4bc4d5fa8c06fd8b33c3ca64b
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-08-27 19:23:01 +1000

    analyze

--- instagram/analyze.sql
+++ instagram/analyze.sql
@@ -21,3 +21,48 @@ SELECT *
 FROM users
 LEFT JOIN photos ON photos.user_id =  users.id
 WHERE photos.id is NULL;
+
+-- We're running a new contest to see who can get the most likes on a single photo. Who won??
+SELECT users.id, users.username
+FROM users
+WHERE users.id = (
+                    SELECT photos.user_id
+                    FROM photos
+                    INNER JOIN likes on likes.photo_id = photos.id
+                    GROUP BY photos.id
+                    ORDER BY COUNT(*) DESC
+                    LIMIT 1
+                  );
+
+-- How many times does the average user post?
+SELECT (
+        SELECT COUNT(*)
+        FROM photos
+      )/(
+          SELECT COUNT(*)
+          FROM users
+        ) AS "average";
+
+-- What are th top 5 most commonly used hashtags?
+SELECT tags.tag_name, temp.total
+FROM tags
+INNER JOIN (
+            SELECT tag_id, COUNT(*) as 'total'
+            FROM photo_tags
+            GROUP BY photo_tags.tag_id
+            ORDER BY COUNT(*) DESC
+            LIMIT 5
+          ) AS temp ON temp.tag_id = tags.id;
+
+-- Find users who have liked every single photo on the site
+SELECT users.id, users.username
+FROM users
+WHERE users.id IN (
+                    SELECT likes.user_id
+                    FROM likes
+                    GROUP BY likes.user_id
+                    HAVING COUNT(*) = ( SELECT COUNT(*) AS 'number of photos'
+                                        FROM photos
+                                      )
+)
+ORDER BY users.id;
